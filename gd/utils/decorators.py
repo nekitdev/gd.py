5d51d2692eef83dee715e01d0d5464f0bcab11cf
Nikita <43587167+NeKitDSS@users.noreply.github.com>
2019-07-14 15:09:46 +0300

    [decorators.py] Removed 'Abstract', reason in desc.
    Removed 'Abstract' because there are some cases where it does not fit.

--- gd/utils/decorators.py
+++ gd/utils/decorators.py
@@ -12,26 +12,4 @@ def benchmark(func):
         return res
     return decorator
 
-class Abstract:
-    def __init__(self):
-        if not hasattr(self, '__to_repr__'):
-            self.__to_repr__ = []
-        if not isinstance(self.__to_repr__, list):
-            raise AttributeError("'Abstract' class expected '__to_repr__' as instance of 'list'. (recieved '%s')" % type(self.__to_repr__).__name__)
-
-    @property
-    def __rmodule__(self):
-        module = self.__module__
-        return module.split('.')[0]
-
-    @property
-    def __name__(self):
-        return self.__class__.__name__
-
-    def __repr__(self):
-        to_repr = [(name, getattr(self, name)) for name in self.__to_repr__]
-        str_repr = ', '.join('%s=%s' % t for t in to_repr)
-        additional = ': ' if len(str_repr) > 0 else ''
-        return '<{0.__rmodule__}.{0.__name__}{1}{2}>'.format(self, additional, str_repr)
-
 # TO_DO: Rename 'decorators.py' to 'wrap_tools.py'
