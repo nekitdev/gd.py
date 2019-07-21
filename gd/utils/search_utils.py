adec589730c35b7365a8cc499d1d04b805230b89
Nikita <43587167+NeKitDSS@users.noreply.github.com>
2019-07-21 19:23:56 +0300

    Update search_utils.py

--- gd/utils/search_utils.py
+++ gd/utils/search_utils.py
@@ -1,40 +1,36 @@
-from .errors import error
+from operator import attrgetter as attrget
 
-def indexall(iterable, item):
-    import builtins
-    data = []
-    item = item.lower() if (type(item) is str) else item
-    res = list(iterable)
-    if any(type(e).__name__ not in dir(builtins) for e in res): # if object is an instance of not built-in class
-        attr = {
-            type(item) is int: 'account_id',
-            type(item) is str: 'name'
-        }.get(True)
-        res = [getattr(elem, attr) for elem in res]
-    for element in res:
-        run_thing(data, res, element, item)
-    return data
+def find(predicate, seq, *, _all=False):
+    res = []
+    # for each element in sequence,
+    # return element if predicate returns True and '_all' is False;
+    # append it to 'res' otherwise.
+    for elem in seq:
+        if predicate(elem):
+            if not _all:
+                return elem
+            res.append(elem)
 
-def run_thing(data, res, elem, item):
-    if type(item) in [int, bool]:
-        if item == elem:
-            data.append(res.index(elem))
-    if type(item) is str:
-        if str(elem).lower().startswith(item):
-            data.append(res.index(elem))
+    return None if not res else res
 
-def search(iterable, **kwargs):
-    _strategy = list(kwargs.keys())[0]
-    _search = list(kwargs.values())[0]
-    if (_strategy is None) or (_search is None):
-        raise error.MissingArguments()
-    mapped = []
-    for i in range(len(iterable)):
-        attr = getattr(iterable[i], _strategy)
-        if callable(attr):
-            mapped.append(attr())
-        else:
-            mapped.append(attr)
-    indexes = indexall(mapped, _search)
-    final_res = [iterable[_index] for _index in indexes]
-    return final_res[0] if len(final_res) is 1 else final_res
+
+def get(iterable, **attrs):
+    # check if ALL elements matching requirements should be returned
+    _all = attrs.pop("_all", False)
+
+    converted = [
+        (attrget(attr.replace('__', '.')), value)
+        for attr, value in attrs.items()
+    ]
+
+    res = []
+    # for each element in iterable,
+    # return element if it matches requirements and '_all' is False;
+    # append it to 'res' otherwise.
+    for elem in iterable:
+        if all(pred(elem) == value for pred, elem in converted):
+            if not _all:
+                return elem
+            res.append(elem)
+
+    return None if not res else res
