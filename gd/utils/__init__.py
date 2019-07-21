cb431a64f2acf60e9bfbab8ceca3009c4181d448
Nikita <43587167+NeKitDSS@users.noreply.github.com>
2019-07-21 19:24:23 +0300

    updated utils.__init__.py a bit

--- gd/utils/__init__.py
+++ gd/utils/__init__.py
@@ -1,2 +1,8 @@
-from .search_utils import search
+import asyncio
+
+from .search_utils import find, get
 from .decorators import benchmark
+
+# for more comfortable coding
+run = asyncio.run
+wait = asyncio.wait
