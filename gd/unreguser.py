c2374d9c35f354720053499329a67b540bf1519e
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-09-13 10:26:32 +0300

    [0.9.4-p] Added gd/unreguser.py 'name' support on __init__.

--- gd/unreguser.py
+++ gd/unreguser.py
@@ -10,9 +10,9 @@ class UnregisteredUser(AbstractEntity):
         ID of :class:`.UnregisteredUser` objects always equals ``0``.
 
     """
-    def __init__(self, id: int = 0):
+    def __init__(self, id: int = 0, name: str = 'UnregisteredUser'):
         super().__init__(id=id)
-        self._name = 'UnregisteredUser'
+        self._name = name
 
     def __repr__(self):
         info = {'id': self.id}
@@ -20,5 +20,5 @@ class UnregisteredUser(AbstractEntity):
 
     @property
     def name(self):
-        """:class:`str`: A name of the user. (``'UnregisteredUser'``)"""
+        """:class:`str`: A name of the user. ``'UnregisteredUser'`` if not present."""
         return self._name
