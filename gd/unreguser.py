a800d08511960ced95bce0d7584e9a545e48085d
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-09-13 10:30:02 +0300

    [0.9.4-p] Fixed UnregisteredUser.

--- gd/unreguser.py
+++ gd/unreguser.py
@@ -4,18 +4,13 @@ from .utils.wrap_tools import make_repr
 class UnregisteredUser(AbstractEntity):
     """Class that represents Unregistered Users in Geometry Dash.
     This class is derived from :class:`.AbstractEntity`.
-
-    .. note::
-
-        ID of :class:`.UnregisteredUser` objects always equals ``0``.
-
     """
     def __init__(self, id: int = 0, name: str = 'UnregisteredUser'):
         super().__init__(id=id)
         self._name = name
 
     def __repr__(self):
-        info = {'id': self.id}
+        info = {'id': self.id, 'name': self.name}
         return make_repr(self, info)
 
     @property
