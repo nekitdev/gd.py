ca1e2b81c64d9a927aa76d48f47403a03f0e63f7
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-10-13 22:05:52 +0300

    [0.10.0] Add improved MapperUtil implementation.

--- gd/utils/mapper.py
+++ gd/utils/mapper.py
@@ -1,34 +1,46 @@
-class mapper_util:
+from .wrap_tools import convert_to_type
+
+__all__ = ('mapper_util', 'MapperUtil', 'pad', 'ceil')
+
+
+class MapperUtil:
+    @staticmethod
     def map(item):
         res = {}
-        for i in range(0, len(item), 2):
-            new = item[i+1]
-            new = new if not _is_str_digit(new) else int(new)
-            res[int(item[i])] = new
+
+        convert = lambda obj: convert_to_type(obj, int)
+
+        for kv_tuple in zip(item[::2], item[1::2]):
+            key, value = map(convert, kv_tuple)
+            res[key] = value
+
         return res
 
+    @staticmethod
     def normalize(item):
         res = str(item).replace('-', '+').replace('_', '/')
-        return _pad(res)
+        return pad(res)
 
+    @staticmethod
     def prepare_sending(item):
         res = str(item).replace('+', '-').replace('/', '_')
-        return _pad(res)
+        return pad(res)
 
-def _pad(res: str):
+mapper_util = MapperUtil()
+
+
+def pad(res: str):
     # pad a string to be divisible by 4
     ilen = len(res)/4
 
     if not ilen.is_integer():
-        n = _ceil(ilen)*4 - len(res)
+        n = ceil(ilen)*4 - len(res)
 
         res += '=' * n
 
     return res
 
-def _ceil(n: float):
+
+def ceil(n: float):
     x = round(n)
     return x+1 if x < n else x
-
-def _is_str_digit(string: str):
-    return string.replace('-', '').isdigit()
