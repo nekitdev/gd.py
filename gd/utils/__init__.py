23fd444fd2a4d79f38e35ec75d399c9f9a048ff7
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-10-13 22:03:28 +0300

    [0.10.0] Remove convert_to_type from __init__.

--- gd/utils/__init__.py
+++ gd/utils/__init__.py
@@ -6,34 +6,3 @@ from .search_utils import *
 from .wrap_tools import *
 
 from . import tasks
-
-
-def convert_to_type(obj: object, try_type: type, on_fail_type: type = None):
-    """A function that tries to convert the given object to a provided type
-
-    Parameters
-    ----------
-    obj: :class:`object`
-        Any object to convert into given type.
-
-    try_type: :class:`type`
-        Type to convert an object to.
-
-    on_fail_type: :class:`type`
-        Type to convert an object on fail.
-        If ``None`` or omitted, returns an ``obj``.
-        On fail returns ``obj`` as well.
-
-    Returns
-    -------
-    `Any`
-        Object of given ``try_type``, on fail of type ``on_fail_type``, and
-        ``obj`` if ``on_fail_type`` is ``None`` or failed to convert.
-    """
-    try:
-        return try_type(obj)
-    except Exception:  # failed to convert
-        try:
-            return on_fail_type(obj)
-        except Exception:
-            return obj
