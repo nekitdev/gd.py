5c00d74339a2cc1e641276e742f3222b5e0475db
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-11-21 01:47:24 +0300

    [0.10.2] Add xml import on __init__.

--- gd/utils/__init__.py
+++ gd/utils/__init__.py
@@ -1,8 +1,10 @@
 import asyncio
 
 from ._async import *
-from .enums import value_to_enum
 from .search_utils import *
 from .wrap_tools import *
+from .xml_parser import *
+
+from .enums import value_to_enum
 
 from . import tasks
