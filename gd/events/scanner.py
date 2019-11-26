971363e8a7cc6d3846e4b57320cdd9d4e55a20b7
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-11-26 10:21:58 +0300

    [0.10.2] Improve loop handling.

--- gd/events/scanner.py
+++ gd/events/scanner.py
@@ -9,7 +9,7 @@ from ..client import Client
 from .. import utils
 
 __all__ = (
-    'AbstractScanner', 'TimelyLevelScanner', 'thread',
+    'AbstractScanner', 'TimelyLevelScanner', 'thread', 'get_loop',
     'daily_listener', 'weekly_listener', 'run', 'all_listeners'
 )
 
@@ -20,6 +20,10 @@ scanner_client = Client(loop=loop)
 log = logging.getLogger(__name__)
 
 all_listeners = []
+
+
+def get_loop():
+    return loop
 
 
 def shutdown_loop(loop):
@@ -68,7 +72,9 @@ thread = threading.Thread(target=run, args=(loop,), name='ScannerThread')
 
 
 class AbstractScanner:
-    def __init__(self, delay: float = 10.0, *, loop=loop):
+    def __init__(self, delay: float = 10.0, *, loop=None):
+        if loop is None:
+            loop = get_loop()
         self.loop = loop
         self.runner = utils.tasks.loop(seconds=delay, loop=loop)(self.main)
         self.cache = None
@@ -119,8 +125,9 @@ class AbstractScanner:
 
 
 class TimelyLevelScanner(AbstractScanner):
-    def __init__(self, t_type: str, delay: int = 10.0, *, loop=loop):
-        super().__init__()
+    def __init__(self, t_type: str, delay: int = 10.0, *, loop=None):
+        super().__init__(delay, loop=loop)
+
         self.type = t_type
         self.method = getattr(scanner_client, 'get_' + t_type)
         self.call_method = 'new_' + t_type
