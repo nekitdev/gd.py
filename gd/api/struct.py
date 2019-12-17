c45b23323da02b05a1ee2224e4f0a4f3a1e98838
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-12-17 11:34:51 +0300

    [0.10.2] Add useful gd.api.get_id function.

--- gd/api/struct.py
+++ gd/api/struct.py
@@ -6,7 +6,77 @@ from .hsv import HSV
 from .parser import *
 from ._property import _object_code, _color_code
 
-__all__ = ('Object', 'ColorChannel', 'Header')
+__all__ = ('Object', 'ColorChannel', 'Header', 'supported', 'get_id')
+
+mapping = {
+    'special': SpecialBlockType,
+    'trigger': TriggerType,
+    'portal': PortalType,
+    'orb': OrbType,
+    'pad': PadType,
+    'easing': Easing,
+    'layer': ZLayer
+}
+
+supported = {name: enum.as_dict() for name, enum in mapping.items()}
+
+d = supported.get('portal')
+for i, s in zip(range(5), ('slow', 'normal', 'fast', 'faster', 'fastest')):
+    d.update({'speed:x{}'.format(i): 'portal:{}speed'.format(s)})
+del i, s, d
+
+
+def _get_dir(directive: str, cls: str, delim: str = ':'):
+    return delim.join((cls, directive.split(delim).pop()))
+
+
+def get_id(x: str, ret_enum: bool = False, delim: str = ':'):
+    """Calculate required value from the given directive ``x``.
+
+    The format is, as follows: ``class:name``, e.g. ``special:h``.
+
+    Parameters
+    ----------
+    x: :class:`str`
+        Directive to get value from.
+
+    ret_enum: :class:`bool`
+        Whether to convert found value to enum. By default, ``False``.
+
+    delim: :class:`str`
+        Character to split given directive string with.
+        It is not recommended to pass this argument to the function.
+
+    Returns
+    -------
+    `Any`
+        The value found, if any.
+
+    Raises
+    ------
+    :exc:`.EditorError`
+        Failed to convert directive to the value.
+    """
+    typeof, name = (
+        string.strip().replace('_', '').lower()
+        for string in x.split(delim, maxsplit=1)
+    )
+
+    try:
+        found = supported[typeof][name]
+
+        if isinstance(found, str) and delim in found:
+            # inner directive
+            return get_id(found)
+
+        elif ret_enum:
+            return mapping[typeof](found)
+
+        return found
+
+    except Exception:
+        raise EditorError('ID by directive {!r} was not found.'.format(x)) from None
+
 
 class Struct:
     dumper = _dump
@@ -79,6 +149,18 @@ class Object(Struct):
     dumper = _object_dump
     convert = _object_convert
     collect = _object_collect
+
+    def set_id(self, directive: str):
+        """Set ``id`` of ``self`` according to the directive, e.g. ``trigger:move``.""
+        self.edit(id=get_id(directive))
+
+    def set_z_layer(self, directive: str):
+        """Set ``z_layer`` of ``self`` according to the directive, e.g. ``layer:t1`` or ``b3``."""
+        self.edit(z_layer=get_id(_get_dir(directive, 'layer'), ret_enum=True))
+
+    def set_easing(self, directive: str):
+        """Set ``easing`` of ``self`` according to the directive, e.g. ``sine_in_out``."""
+        self.edit(easing=get_id(_get_dir(directive, 'easing'), ret_enum=True))
 
     def set_color(self, color):
         """Set ``rgb`` of ``self`` to ``color``."""
