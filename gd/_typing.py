ace77eef040f49603825a6f5b94dfcc15cda2d92
NeKitDS <nekitguypro@gmail.com>
2020-01-10 07:27:59 +0300

    [0.10.3] Make _typing even better.

--- gd/_typing.py
+++ gd/_typing.py
@@ -22,6 +22,12 @@ from typing import (
     no_type_check,
     overload,
 )
+try:  # pragma: no cover
+    from typing import ForwardRef as ref  # type: ignore
+except ImportError:  # pragma: no cover
+    # python < 3.7
+    from typing import _ForwardRef as ref  # type: ignore
+
 from enum import Enum
 
 __all__ = (
@@ -78,31 +84,38 @@ __all__ = (
     'overload',
 )
 
-AbstractEntity = 'gd.abstractentity.AbstractEntity'
-AbstractUser = 'gd.abstractuser.AbstractUser'
-LevelRecord = 'gd.abstractuser.LevelRecord'
-Client = 'gd.client.Client'
-Color = 'gd.colors.Color'
-Comment = 'gd.comment.Comment'
-FriendRequest = 'gd.friend_request.FriendRequest'
-IconSet = 'gd.iconset.IconSet'
-Level = 'gd.level.Level'
-Gauntlet = 'gd.level_packs.Gauntlet'
-MapPack = 'gd.level_packs.MapPack'
-Message = 'gd.message.Message'
-Song = 'gd.song.Song'
-UserStats = 'gd.user.UserStats'
-User = 'gd.user.User'
-NEnum = 'gd.utils.enums.NEnum'
-Filters = 'gd.utils.filters.Filters'
-Parameters = 'gd.utils.params.Parameters'
-Loop = 'gd.utils.tasks.Loop'
-Editor = 'gd.api.editor.Editor'
-HSV = 'gd.api.hsv.HSV'
-LevelCollection = 'gd.api.save.LevelCollection'
-Struct = 'gd.api.struct.Struct'
-Object = 'gd.api.struct.Object'
-ColorChannel = 'gd.api.struct.ColorChannel'
-ColorCollection = 'gd.api.struct.ColorCollection'
-Header = 'gd.api.struct.Header'
-LevelAPI = 'gd.api.struct.LevelAPI'
+
+def __repr__(forward_ref):
+    return str(forward_ref.__forward_arg__)
+
+
+setattr(ref, '__repr__', __repr__)
+
+AbstractEntity = ref('gd.abstractentity.AbstractEntity')
+AbstractUser = ref('gd.abstractuser.AbstractUser')
+LevelRecord = ref('gd.abstractuser.LevelRecord')
+Client = ref('gd.client.Client')
+Color = ref('gd.colors.Color')
+Comment = ref('gd.comment.Comment')
+FriendRequest = ref('gd.friend_request.FriendRequest')
+IconSet = ref('gd.iconset.IconSet')
+Level = ref('gd.level.Level')
+Gauntlet = ref('gd.level_packs.Gauntlet')
+MapPack = ref('gd.level_packs.MapPack')
+Message = ref('gd.message.Message')
+Song = ref('gd.song.Song')
+UserStats = ref('gd.user.UserStats')
+User = ref('gd.user.User')
+NEnum = ref('gd.utils.enums.NEnum')
+Filters = ref('gd.utils.filters.Filters')
+Parameters = ref('gd.utils.params.Parameters')
+Loop = ref('gd.utils.tasks.Loop')
+Editor = ref('gd.api.editor.Editor')
+HSV = ref('gd.api.hsv.HSV')
+LevelCollection = ref('gd.api.save.LevelCollection')
+Struct = ref('gd.api.struct.Struct')
+Object = ref('gd.api.struct.Object')
+ColorChannel = ref('gd.api.struct.ColorChannel')
+ColorCollection = ref('gd.api.struct.ColorCollection')
+Header = ref('gd.api.struct.Header')
+LevelAPI = ref('gd.api.struct.LevelAPI')
