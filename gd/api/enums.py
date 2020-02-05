c786549ff213a6af2f01204f1e35ff8769dfdf64
Nikita <43587167+NeKitDS@users.noreply.github.com>
2020-02-05 18:56:52 +0300

    [0.10.4-p] Remove commented @stringify parts.

--- gd/api/enums.py
+++ gd/api/enums.py
@@ -1,4 +1,3 @@
-# from ..utils.decorators import stringify
 from ..utils.enums import NEnum
 
 __all__ = (
@@ -30,7 +29,6 @@ __all__ = (
 )
 
 
-# @stringify(int)
 class ObjectDataEnum(NEnum):
     ID = 1
     X = 2  # in Units
@@ -235,7 +233,6 @@ class ObjectDataEnum(NEnum):
     LOCK_TO_CAMERA_Y = -303
 
 
-# @stringify(int)
 class ColorChannelProperties(NEnum):
     RED = 1
     GREEN = 2
