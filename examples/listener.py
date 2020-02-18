3df25509df84adc7f876ec32b0138e7d88106580
Nikita <43587167+NeKitDS@users.noreply.github.com>
2020-02-18 13:35:33 +0300

    [0.10.4] Add listener example.

---
+++ examples/listener.py
@@ -0,0 +1,21 @@
+"""An example showing the use of listeners.
+Author: NeKitDS
+"""
+
+import gd
+
+# enable logging
+gd.setup_logging()
+
+# create a client
+client = gd.Client()
+
+# create a listener object and decorate as an event
+@client.listen_for('rate')
+# implement the listener handling
+async def on_level_rated(level: gd.Level) -> None:
+    print('New level rated: {}'.format(level.name))
+
+
+# enable listening
+gd.events.start()
