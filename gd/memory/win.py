ca905dbcaacc4da333a88da42688b0d6ef09107a
Nikita Tikhonov <nekitguypro@gmail.com>
2020-06-08 00:25:55 +0300

    [1.0.0a0] Implement StructureMeta in order to use type annotations.

--- gd/memory/win.py
+++ gd/memory/win.py
@@ -1,6 +1,8 @@
 from ctypes import wintypes
 import ctypes
 
+from gd.memory.utils import Structure
+
 kernel32 = ctypes.WinDLL("kernel32.dll")
 user32 = ctypes.WinDLL("user32.dll")
 
@@ -14,20 +16,17 @@ PAGE_EXECUTE_READWRITE = 0x40
 JMP = bytes([0xE9])
 
 
-class ProcessEntry32(ctypes.Structure):
-    fields = dict(
-        dwSize=wintypes.DWORD,
-        cntUsage=wintypes.DWORD,
-        th32ProcessID=wintypes.DWORD,
-        th32DefaultHeapID=ctypes.POINTER(ctypes.c_ulong),
-        th32ModuleID=wintypes.DWORD,
-        cntThreads=wintypes.DWORD,
-        th32ParentProcessID=wintypes.DWORD,
-        pcPriClassBase=wintypes.LONG,
-        dwFlags=wintypes.DWORD,
-        szExeFile=ctypes.c_char * wintypes.MAX_PATH,
-    )
-    _fields_ = list(fields.items())
+class ProcessEntry32(Structure):
+    dwSize: wintypes.DWORD
+    cntUsage: wintypes.DWORD
+    th32ProcessID: wintypes.DWORD
+    th32DefaultHeapID: ctypes.POINTER(ctypes.c_ulong)
+    th32ModuleID: wintypes.DWORD
+    cntThreads: wintypes.DWORD
+    th32ParentProcessID: wintypes.DWORD
+    pcPriClassBase: wintypes.LONG
+    dwFlags: wintypes.DWORD
+    szExeFile: ctypes.c_char * wintypes.MAX_PATH
 
     @property
     def name(self) -> str:
@@ -38,20 +37,17 @@ class ProcessEntry32(ctypes.Structure):
         self.dwSize = ctypes.sizeof(self)
 
 
-class ModuleEntry32(ctypes.Structure):
-    fields = dict(
-        dwSize=wintypes.DWORD,
-        th32ModuleID=wintypes.DWORD,
-        th32ProcessID=wintypes.DWORD,
-        GlblcntUsage=wintypes.DWORD,
-        ProccntUsage=wintypes.DWORD,
-        modBaseAddr=ctypes.POINTER(wintypes.BYTE),
-        modBaseSize=wintypes.DWORD,
-        hModule=wintypes.HMODULE,
-        szModule=ctypes.c_char * MAX_MODULE_NAME32,
-        szExePath=ctypes.c_char * wintypes.MAX_PATH,
-    )
-    _fields_ = list(fields.items())
+class ModuleEntry32(Structure):
+    dwSize: wintypes.DWORD
+    th32ModuleID: wintypes.DWORD
+    th32ProcessID: wintypes.DWORD
+    GlblcntUsage: wintypes.DWORD
+    ProccntUsage: wintypes.DWORD
+    modBaseAddr: ctypes.POINTER(wintypes.BYTE)
+    modBaseSize: wintypes.DWORD
+    hModule: wintypes.HMODULE
+    szModule: ctypes.c_char * MAX_MODULE_NAME32
+    szExePath: ctypes.c_char * wintypes.MAX_PATH
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
