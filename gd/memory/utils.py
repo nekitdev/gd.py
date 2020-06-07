8f15f20a60a2bc1e3d42d47a52e3e42b40b3cb82
Nikita Tikhonov <nekitguypro@gmail.com>
2020-06-08 00:29:33 +0300

    [1.0.0a0] Add missing gd.memory.utils.

---
+++ gd/memory/utils.py
@@ -0,0 +1,29 @@
+import ctypes
+from typing import Any, Dict, Tuple, Type
+
+
+class StructureMeta(type(ctypes.Structure)):
+    def __new__(
+        meta_cls, name: str, bases: Tuple[Type[Any]], namespace: Dict[str, Any]
+    ) -> Type[ctypes.Structure]:
+        cls = super().__new__(meta_cls, name, bases, namespace)
+
+        fields = {}
+
+        for base in reversed(cls.mro()):
+            try:
+                fields.update(cls.__annotations__)
+            except AttributeError:
+                pass
+
+        print(fields)
+
+        cls._fields_ = list(fields.items())
+
+        return cls
+
+
+class Structure(ctypes.Structure, metaclass=StructureMeta):
+    """Structure that has ability to populate its fields with annotations."""
+
+    pass
