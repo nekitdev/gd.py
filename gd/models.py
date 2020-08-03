1ff61df9a13a020fca079e942e0ac7900c1d8397
Nikita Tikhonov <NeKitDS@users.noreply.github.com>
2020-08-03 14:06:46 +0300

    [1.0.0rc0] Start working on models.

---
+++ gd/models.py
@@ -0,0 +1,24 @@
+from gd.typing import TypeVar
+
+T = TypeVar("T")
+
+
+class Singleton:
+    instance = None
+
+    def __repr__(self) -> str:  # pragma: no cover
+        return make_repr(self)
+
+    @classmethod
+    def __new__(cls, *args, **kwargs) -> T:
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+            cls.instance.__init__(*args, **kwargs)
+        return cls.instance
+
+
+class NULL(Singleton):
+    pass
+
+
+null = NULL()
