ccef8383f9f7b8b45babeec4e23e6d513148ef76
Nikita Tikhonov <nekitdevofficial@gmail.com>
2021-01-02 22:22:20 +0300

    [1.0.0rc1] Fix gd.server imports.

--- gd/server/__init__.py
+++ gd/server/__init__.py
@@ -2,9 +2,9 @@ try:
     from gd.server.common import *
     from gd.server.cooldown import *
     from gd.server.core import *
-    from gd.server.fields import *
     from gd.server.handler import *
     from gd.server.middlewares import *
+    from gd.server.routes import *
     from gd.server.typing import *
     from gd.server.user import *
     from gd.server.utils import *
