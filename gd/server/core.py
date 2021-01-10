2b55042dc948d731b514a198e21b0bb13fcf4594
Nikita Tikhonov <nekitdevofficial@gmail.com>
2021-01-10 13:09:28 +0300

    [1.0.0rc1] Fix token database creation.

--- gd/server/core.py
+++ gd/server/core.py
@@ -7,7 +7,7 @@ import gd
 from gd.server.common import web
 from gd.server.middlewares import status_error_middleware
 from gd.server.routes import routes
-from gd.server.token import Token, TokenDatabase
+from gd.server.token import TokenDatabase
 from gd.server.typing import Middleware
 from gd.typing import Iterable, Optional, Protocol
 
@@ -63,7 +63,7 @@ async def create_app(
 
     app.client = client
 
-    app.token_database = app.token_db = TokenDatabase()
+    app.token_database = TokenDatabase()
 
     app.add_routes(routes)
 
