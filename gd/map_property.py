e041ad70e1fe6f4f02f4007b2e8c538db1388343
Nikita Tikhonov <nekitdevofficial@gmail.com>
2021-03-20 01:39:28 +0300

    [1.0.0rc1] Rewrite map_property.

--- gd/map_property.py
+++ gd/map_property.py
@@ -1,49 +1,129 @@
-from gd.code_utils import get_frame
-from gd.typing import Any, Dict, Optional, TypeVar
+import linecache
+import secrets
 
-__all__ = ("map_property", "is_null")
+from gd.code_utils import get_frame
+from gd.typing import Any, Dict, Iterable, Optional, Type, TypeVar
+
+__all__ = ("map_property",)
 
 T = TypeVar("T")
 U = TypeVar("U")
-V = TypeVar("V")
 
-TEMPLATE = """
-def get_{name}(self) -> Optional[TYPE]:
+EMPTY = ""
+EXECUTE = "exec"
+
+OPTIONAL = "Optional"
+
+INCLUDE = {OPTIONAL: Optional}
+
+FILE_NAME = "<generated {function}_{token}>"
+FUNCTION = "{method}_{name}"
+
+DOC = "__doc__"
+
+SIZE = 4
+
+GET = "get"
+
+GET_TEMPLATE = """
+def {function}(self) -> Optional[TYPE]:
     return self.{attr}.get({key!r})
+""".strip()
 
 
-def set_{name}(self, {name}: TYPE) -> None:
+GET_OR_DEFAULT = "get_or_default"
+
+GET_OR_DEFAULT_TEMPLATE = """
+def {function}(self) -> TYPE:
+    return self.{attr}.get({key!r}, {default!r})
+""".strip()
+
+
+SET = "set"
+
+SET_TEMPLATE = """
+def {function}(self, {name}: TYPE) -> None:
     self.{attr}[{key!r}] = {name}
+""".strip()
 
 
-def delete_{name}(self) -> None:
-    try:
-        del self.{attr}[{key!r}]
+DELETE = "delete"
 
-    except KeyError:
-        pass
+DELETE_TEMPLATE = """
+def {function}(self) -> None:
+    mapping = self.{attr}
+    key = {key!r}
+
+    if key in mapping:
+        del mapping[key]
+""".strip()
 
 
-{name} = property(get_{name}, set_{name}, delete_{name})
-"""
+METHOD_TO_TEMPLATE = {
+    GET: GET_TEMPLATE,
+    GET_OR_DEFAULT: GET_OR_DEFAULT_TEMPLATE,
+    SET: SET_TEMPLATE,
+    DELETE: DELETE_TEMPLATE,
+}
+
+
+def compile_method(
+    method: str,  # either "get", "get_or_default", "set" or "delete"
+    name: str,  # name to use
+    attr: str,  # attribute to fetch the mapping from
+    type: Type[T],  # type of the value
+    key: U,  # such that eval(repr(key)) <==> key
+    default: Optional[T],  # such that eval(repr(default)) <==> default
+    namespace: Optional[Dict[str, Any]] = None,
+) -> Any:
+    function = FUNCTION.format(method=method, name=name)
+
+    source = METHOD_TO_TEMPLATE[method].format(
+        function=function, name=name, attr=attr, key=key, default=default
+    )
+
+    if namespace is None:
+        namespace = {}
+
+    environment = INCLUDE.copy()
+
+    environment.update(namespace, TYPE=type)  # type: ignore
+
+    if namespace is not None:
+        environment.update(namespace)
+
+    file_name = FILE_NAME.format(function=function, token=secrets.token_hex(SIZE))
+
+    linecache.cache[file_name] = (  # type: ignore
+        len(source),
+        None,  # timestamp
+        source.splitlines(True),  # include line endings
+        file_name,
+    )
+
+    code = compile(source, file_name, EXECUTE)
+
+    exec(code, environment)
+
+    return environment[function]
 
 
 def map_property(
     name: str,
     attr: str,
-    key: T,  # such that eval(repr(key)) == key
+    type: Type[T],
+    key: U,
+    default: Optional[T] = None,
     *,
-    type: Any = Any,
     doc: Optional[str] = None,
-    # namespace to use, tries to fetch from caller frame if not given
-    namespace: Optional[Dict[str, V]] = None,
+    namespace: Optional[Dict[str, Any]] = None,
+    no_default: Iterable[str] = (GET, SET, DELETE),
+    or_default: Iterable[str] = (GET_OR_DEFAULT, SET, DELETE),
 ) -> property:
-    env: Dict[str, Any] = {}
-
     if namespace is None:
         namespace = {}
 
-        try:  # ayy frame hacks! ~ nekit
+        try:  # ayy, frame hacks! ~ nekit
             frame = get_frame(1)
 
             namespace.update(frame.f_globals)
@@ -54,17 +134,11 @@ def map_property(
         except (AttributeError, ValueError):
             pass
 
-    env.update(namespace, TYPE=type, Optional=Optional)
+    generated = property(
+        *(
+            compile_method(method, name, attr, type, key, default, namespace)
+            for method in (no_default if default is None else or_default)
+        )
+    )
 
-    code = TEMPLATE.format(name=name, attr=attr, key=key)
-
-    exec(code, env)
-
-    some_property = env[name]
-    some_property.__doc__ = doc
-
-    return some_property
-
-
-def is_null(some: Optional[T]) -> bool:
-    return some is None
+    return generated
