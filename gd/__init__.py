2f16e5a0d225bafd9bd129d071e68e056b5061d8
Nikita Tikhonov <nekitdevofficial@gmail.com>
2022-08-16 14:19:13 +0300

    Add all imports.

--- gd/__init__.py
+++ gd/__init__.py
@@ -11,10 +11,10 @@ __version__ = "1.0.0-alpha.1"
 from gd import api, encoding, events, image, json, memory, tasks
 from gd.artist import Artist
 
-# from gd.client import Client
+from gd.client import Client
 from gd.colors import Color
 
-# from gd.comments import Comment, LevelComment, UserComment
+from gd.comments import Comment, LevelComment, UserComment
 from gd.credentials import Credentials
 from gd.entity import Entity
 from gd.enums import (
@@ -104,18 +104,18 @@ from gd.errors import (
 )
 from gd.filters import Filters
 
-# from gd.friend_request import FriendRequest
-# from gd.http import HTTPClient
-# from gd.level import Level
-# from gd.level_packs import Gauntlet, MapPack
-# from gd.message import Message
+from gd.friend_request import FriendRequest
+from gd.http import HTTPClient
+from gd.level import Level
+from gd.level_packs import Gauntlet, MapPack
+from gd.message import Message
 from gd.password import Password
 
-# from gd.platform import SYSTEM_BITS, SYSTEM_PLATFORM, SYSTEM_PLATFORM_CONFIG, PlatformConfig
-# from gd.relationship import Relationship
-# from gd.rewards import Chest, Quest
-# from gd.song import Song
-# from gd.user import User
+from gd.platform import SYSTEM_BITS, SYSTEM_PLATFORM, SYSTEM_PLATFORM_CONFIG, PlatformConfig
+from gd.relationship import Relationship
+from gd.rewards import Chest, Quest
+from gd.song import Song
+from gd.user import User
 from gd.version import python_version_info, version_info
 from gd.versions import GameVersion, Version
 
