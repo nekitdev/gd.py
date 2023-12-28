f1f9b2742ba9419b17b2095fffab80ee1c9f365c
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-12-28 22:02:08 +0300

    Add new modules.

---
+++ gd/cache.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, TypeVar
+
+from attrs import define, field
+
+if TYPE_CHECKING:
+    from gd.artist import Artist
+    from gd.comments import LevelComment, UserComment
+    from gd.friend_request import FriendRequest
+    from gd.level_packs import Gauntlet, MapPack
+    from gd.levels import Level
+    from gd.message import Message
+    from gd.songs import Song
+    from gd.users import User
+
+__all__ = ("CacheSettings", "Cache", "CachePart")
+
+DEFAULT_SIZE = 0
+
+
+@define()
+class CacheSettings:
+    artist_size: int = DEFAULT_SIZE
+    user_comment_size: int = DEFAULT_SIZE
+    level_comment_size: int = DEFAULT_SIZE
+    friend_request_size: int = DEFAULT_SIZE
+    gauntlet_size: int = DEFAULT_SIZE
+    map_pack_size: int = DEFAULT_SIZE
+    level_size: int = DEFAULT_SIZE
+    message_size: int = DEFAULT_SIZE
+    song_size: int = DEFAULT_SIZE
+    user_size: int = DEFAULT_SIZE
+
+
+T = TypeVar("T")
+
+CachePart = Dict[int, T]
+
+
+@define()
+class Cache:
+    settings: CacheSettings = field(factory=CacheSettings)
+
+    artists: CachePart[Artist] = field(factory=dict)
+    user_comments: CachePart[UserComment] = field(factory=dict)
+    level_comments: CachePart[LevelComment] = field(factory=dict)
+    friend_requests: CachePart[FriendRequest] = field(factory=dict)
+    gauntlets: CachePart[Gauntlet] = field(factory=dict)
+    map_packs: CachePart[MapPack] = field(factory=dict)
+    levels: CachePart[Level] = field(factory=dict)
+    messages: CachePart[Message] = field(factory=dict)
+    songs: CachePart[Song] = field(factory=dict)
+    users: CachePart[User] = field(factory=dict)
---
+++ gd/queries.py
@@ -0,0 +1,50 @@
+from typing import final
+
+from attrs import frozen
+from iters.iters import iter
+from typing_aliases import DynamicTuple
+from typing_extensions import Self
+from gd.models_constants import QUERY_SEPARATOR
+
+from gd.models_utils import concat_query, split_query
+from gd.robtop import RobTop
+from gd.typing import IntString
+
+__all__ = ("EMPTY_QUERY", "QueryPart", "QueryParts", "Query", "query")
+
+QueryPart = IntString
+QueryParts = DynamicTuple[QueryPart]
+
+
+def parse_query_part(string: str) -> QueryPart:
+    try:
+        return int(string)
+
+    except ValueError:
+        return string
+
+
+@final
+@frozen()
+class Query(RobTop):
+    parts: QueryParts
+
+    @classmethod
+    def from_robtop(cls, string: str) -> Self:
+        parts = iter(split_query(string)).map(parse_query_part).tuple()
+
+        return cls(parts)
+
+    def to_robtop(self) -> str:
+        return iter(self.parts).map(str).collect(concat_query)
+
+    @classmethod
+    def can_be_in(cls, string: str) -> bool:
+        return QUERY_SEPARATOR in string
+
+
+def query(*parts: QueryPart) -> Query:
+    return Query(parts)
+
+
+EMPTY_QUERY = query()
